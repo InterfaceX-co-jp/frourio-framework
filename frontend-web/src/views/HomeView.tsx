f41076f2339725f5b6ba973689d1e976ec3e9030
mikana0918 <shogo.kusuhara@gmail.com>
2025-06-11 18:13:15 +0900

    HomeViewコンポーネントにHealthcheck機能を追加

--- frontend-web/src/views/HomeView.tsx
+++ frontend-web/src/views/HomeView.tsx
@@ -1,10 +1,25 @@
+import { defaultWithoutAuthApiClient } from '@/utils/apiClient'
 import { Button } from 'flowbite-react'
+import { toast } from 'react-toastify'
 
 export default function HomeView() {
+  const handleHealthcheck = () => {
+    defaultWithoutAuthApiClient.health
+      .$get()
+      .then((response) => {
+        console.log('Healthcheck response:', response)
+        toast.success('Healthcheck successful!')
+      })
+      .catch((error) => {
+        console.error('Healthcheck error:', error)
+        toast.error('Healthcheck failed!')
+      })
+  }
+
   return (
     <div>
       <div>
-        <Button>Healthcheck</Button>
+        <Button onClick={handleHealthcheck}>Healthcheck</Button>
       </div>
     </div>
   )
